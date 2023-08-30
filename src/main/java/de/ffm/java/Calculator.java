29cad8200a022cac584ee876a725daf0302329ff
UlrikeWerner <ulrike_werner@web.de>
2023-08-30 15:54:17 +0200

    add Calculator class

---
+++ src/main/java/de/ffm/java/Calculator.java
@@ -0,0 +1,22 @@
+package de.ffm.java;
+
+public class Calculator {
+    public static int add(int a, int b){
+        return a + b;
+    }
+    public static int sub(int a, int b){
+        return a - b;
+    }
+    public static int multi(int a, int b){
+        return a * b;
+    }
+    public static double div(int a, int b){
+        return (double)a / (double)b;
+    }
+    public static int mod(int a, int b){
+        return a % b;
+    }
+    public static int xor(int a, int b){
+        return a ^ b;
+    }
+}
